2b8157e2b23887212b175b778696d10674d81337
wh1tepr1nc3 <amamamania@mail.ru>
2024-07-14 11:01:56 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#1st program
+print(9 ** 0.5 * 5)
+#2st program
+print (9.99 > 9.98 and 1000 != 1000.1)
+#3st program
+a = 1234%1000//10
+b = 5678%1000//10
+c = a+b
+print(c)
+#4st program
+a, b = 13.42, 42.13
+ai, bi = int(a), int(b)
+af, bf = ((a-ai) * 100), int((b - bi) * 100)
+print(ai == bf or bi == af)
